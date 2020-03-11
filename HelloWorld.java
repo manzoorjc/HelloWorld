5f832990a03568489f237bfe9c72c43a93d482c1
root <root@ip-172-31-24-179.us-east-2.compute.internal>
2020-03-11 09:07:04 +0000

    Last check

--- HelloWorld.java
+++ HelloWorld.java
@@ -7,7 +7,7 @@ class HelloWorld
     // Prints "Hello, World" to the terminal window. 
     public static void main(String args[]) 
     { 
-        System.out.println("Hello, World using jenkins"); 
+        System.out.println("Hello, World using jenkins, this is my another test"); 
     } 
 } 
 
